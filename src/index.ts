947ceea8105ed87a1029dd3a9dfdcfa5180f5638
Your Name <you@example.com>
2024-06-03 16:01:35 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { graphql } from "graphql";
+import { PrismaClient } from "./lib/db";
 
 async function startServer() {
   const app = express();
@@ -15,11 +16,41 @@ async function startServer() {
             hello: String
             say(name: String): String
         }
+        type Mutation {
+          createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+        }
     `,
     resolvers: {
       Query: {
         hello: () => `hey there, I am a gql server`,
         say: (_, { name }: { name: string }) => `hey ${name}`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await PrismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
+        },
       },
     },
   });
---
+++ src/lib/db.ts
@@ -0,0 +1,3 @@
+import { PrismaClient as ImportedPrismaClient } from "@prisma/client";
+
+export const PrismaClient = new ImportedPrismaClient();
